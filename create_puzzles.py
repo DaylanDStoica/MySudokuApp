619ff9564c19ab9d3b8462a71ca462ba510b969a
DaylanDStoica <daylanstoica@gmail.com>
2026-03-11 13:28:52 -0700

    create base of create_puzzle_random function, and add the valid values that can go in a cell

--- create_puzzles.py
+++ create_puzzles.py
@@ -7,9 +7,16 @@ EMPTY_CELL = '_'
 EMPTY_ROW = [EMPTY_CELL] * 9
 EMPTY_BOARD = [EMPTY_ROW] * 9 # array of arrarys
 
+VALID_CELL_VALUES = [ '1', '2', '3', '4', '5', '6', '7', '8', '9']
 def print_board(board): # prints the board in a readable format, with spaces between the cells and without the list brackets and commas
     for row in board:
         print(' '.join(row))
 
 # print_board(EMPTY_BOARD)
 
+def create_puzzle_random():
+    # through row creation, fill up a board
+    # then check the board for rule compliance, and if it is not compliant, start over
+    # this is a brute-force method, and it may not be the most efficient way to
+
+    board = EMPTY_BOARD
