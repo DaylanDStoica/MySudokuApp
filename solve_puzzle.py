0cc224926058e5822f15a469f75c1205884865ec
DaylanDStoica <daylanstoica@gmail.com>
2026-04-22 12:40:47 -0700

    begin writing the read from puzzle function within solve_puzzle.py. Two bugs are prominent in that reading seems to be generating a new puzzle into the test file, and that not all puzzle lines are being recorded

--- solve_puzzle.py
+++ solve_puzzle.py
@@ -9,6 +9,49 @@ from create_puzzles import is_box_valid, is_col_valid, is_row_valid, is_puzzle_v
 from create_puzzles import EMPTY_CELL
 ACCEPTABLE_CELL_VALUES = [1,2,3,4,5,6,7,8,9]
 
+
+# testing puzzle format, by reading from a premade puzzle file
+def read_puzzle_from_file( filename = "playable_puzzles.txt"):
+    # print(" beginning file reading")
+    f = open(filename, "r") # get contents of the file
+    # print(" file read")
+    filecontent = f.read() # read through the contents, as a string
+    # print( " file contents written down")
+    filecontent = filecontent.split('\n') # splits the file readings into each row being its own string
+    # f.close()
+    puzzle = ""
+    # going through the filecontent, remove all chars or lines that does not belong in a cell ( 0-9 or EMPTY_CELL)
+    # print(filecontent)
+    # set up for loop that grabs entire rows, not just individual chars
+    for row in filecontent: 
+        # print("row:", row)
+        if row == "" : # if the row is an empty row ( not even spaces, just empty)
+            continue
+        # if the row starts with spaces ' ', remove the spaces until you get a non-space char
+        while ( row[0] == ' '):
+            row[0] = ''
+        # if the start of the row shows it is a cell row, copy it into puzzle value
+        if ( row[0] in ACCEPTABLE_CELL_VALUES) or ( row[0] == EMPTY_CELL):
+            puzzle += row + '\n'
+            # print(f"adding %s to puzzle", row)
+            # print(f"puzzle is now: %s", puzzle)
+        else:
+            # print( f"skipping row copy because %s", row)
+            continue
+    # puzzle = puzzle.split('\n')
+    print("puzzle \n", puzzle)
+    f.close()
+
+print("read from file function")
+for i in range(3): 
+    read_puzzle_from_file()
+
+# BUG: each running of the read from file function, seems to be putting in new puzzles into the txt file
+# this would explain the lag in time, as each calling is accidentally generating a new puzzle
+# TODO: fix that, reading should not be overwriting
+# BUG: the outputted puzzle is outputting less than 9 lines of puzzle , instead of the standard 9 lines
+# TODO: fix that, 
+
 # puzzle format of 2D array, puzzle[9][9]
 def findTheNextEmptyCell ( unsolved_puzzle):
     # return the coordinates of the next empty cell within the puzzle, moving left-right top-down
@@ -39,8 +82,9 @@ def solvePuzzleV1( unsolved_puzzle):
         if empty_cell_coords == (-1,-1):
             if is_puzzle_valid(puzzle_copy):
                 return True
-            else: # reached the end and the puzzle is NOT valid, error
+            else: # reached the end and the puzzle is NOT valid? error
                 print("ERROR: reached end of puzzle, puzzle is invalid")
+                return False 
 
         # Step 2: loop through the list of viable numbers to insert into the empty cell
         temp_puzzle = puzzle_copy.copy() # create a puzzle copy for verifying new values
@@ -52,4 +96,4 @@ def solvePuzzleV1( unsolved_puzzle):
         # if after going through all numbers for the cell, and still no valid numbers
         # the empty cell will remain empty
         # then, retrace to a prior attempted cell, skipping over the cells hard-written upon puzzle reception
-        
+
